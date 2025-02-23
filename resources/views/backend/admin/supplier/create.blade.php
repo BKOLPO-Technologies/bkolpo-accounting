0f2c746a5a837cca1a09b92dcd879d036552e213
RJ ZIM <rjzim01@gmail.com>
2025-02-23 16:24:54 +0600

    page_update_001

--- resources/views/backend/admin/supplier/create.blade.php
+++ resources/views/backend/admin/supplier/create.blade.php
@@ -1,14 +1,12 @@
 @extends('layouts.admin')
 @section('admin')
 
-<!-- Content Wrapper. Contains page content -->
 <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
     <section class="content-header">
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6">
-                    <!-- <h1>DataTables</h1> -->
+                    <h1>Supplier</h1>
                 </div>
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
@@ -18,26 +16,21 @@
                     </ol>
                 </div>
             </div>
-        </div><!-- /.container-fluid -->
+        </div>
     </section>
 
-    <!-- Main content -->
     <section class="content">
         
       <div class="container-fluid">
         <div class="row">
-          <!-- left column -->
+          
           <div class="col-md-12">
-            <!-- general form elements -->
-            <div class="card card-secondary">
+            
+            <div class="card card-primary card-outline shadow-lg">
               <div class="card-header">
                 <h3 class="card-title">Add New supplier Details</h3>
-                <!-- <br>
-                <span style="font-size: 12px;">Put Branch Manage Information</span> -->
               </div>
               
-              <!-- /.card-header -->
-              <!-- form start -->
               <form role="form" action="{{ route('admin.supplier.store') }}" method="POST">
                 @csrf
 
@@ -45,14 +38,28 @@
                     <div class="row">
                         <div class="col-md-6">
                             <div class="form-group">
-                                <label>Name</label>
-                                <input type="text" class="form-control" placeholder="Name" name="name" required>
+                                <label>Name
+                                    @error('name')
+                                        <span class="text-danger">{{ $message }}</span>
+                                    @enderror
+                                </label>
+                                <div class="input-group">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text"><i class="fas fa-id-card"></i></span>
+                                    </div>
+                                    <input type="text" class="form-control" placeholder="Enter Supplier Name" name="name" required>
+                                </div>
                             </div>
                         </div>
                         <div class="col-md-6">
                             <div class="form-group">
                                 <label>Company</label>
-                                <input type="text" class="form-control" placeholder="Company Name" name="company">
+                                <div class="input-group">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text"><i class="fas fa-building"></i></span>
+                                    </div>
+                                    <input type="text" class="form-control" placeholder="Company Name" name="company">
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -61,13 +68,23 @@
                         <div class="col-md-6">
                             <div class="form-group">
                                 <label>Phone</label>
-                                <input type="number" class="form-control" placeholder="Phone" name="phone">
+                                <div class="input-group">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text"><i class="fas fa-phone"></i></span>
+                                    </div>
+                                    <input type="number" class="form-control" placeholder="Phone" name="phone">
+                                </div>
                             </div>
                         </div>
                         <div class="col-md-6">
                             <div class="form-group">
                                 <label>Email</label>
-                                <input type="email" class="form-control" placeholder="Email Address" name="email">
+                                <div class="input-group">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text"><i class="fas fa-envelope"></i></span>
+                                    </div>
+                                    <input type="email" class="form-control" placeholder="Email Address" name="email">
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -76,13 +93,23 @@
                         <div class="col-md-6">
                             <div class="form-group">
                                 <label>Address</label>
-                                <input type="text" class="form-control" placeholder="Address" name="address">
+                                <div class="input-group">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text"><i class="fas fa-map-marker-alt"></i></span>
+                                    </div>
+                                    <input type="text" class="form-control" placeholder="Address" name="address">
+                                </div>
                             </div>
                         </div>
                         <div class="col-md-6">
                             <div class="form-group">
                                 <label>City</label>
-                                <input type="text" class="form-control" placeholder="City" name="city">
+                                <div class="input-group">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text"><i class="fas fa-city"></i></span>
+                                    </div>
+                                    <input type="text" class="form-control" placeholder="City" name="city">
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -91,13 +118,23 @@
                         <div class="col-md-6">
                             <div class="form-group">
                                 <label>Region</label>
-                                <input type="text" class="form-control" placeholder="Region" name="region">
+                                <div class="input-group">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text"><i class="fas fa-globe"></i></span>
+                                    </div>
+                                    <input type="text" class="form-control" placeholder="Region" name="region">
+                                </div>
                             </div>
                         </div>
                         <div class="col-md-6">
                             <div class="form-group">
                                 <label>Country</label>
-                                <input type="text" class="form-control" placeholder="Country" name="country">
+                                <div class="input-group">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text"><i class="fas fa-flag"></i></span>
+                                    </div>
+                                    <input type="text" class="form-control" placeholder="Country" name="country">
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -106,19 +143,28 @@
                         <div class="col-md-6">
                             <div class="form-group">
                                 <label>PostBox</label>
-                                <input type="text" class="form-control" placeholder="Post Box" name="postbox">
+                                <div class="input-group">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text"><i class="fas fa-inbox"></i></span>
+                                    </div>
+                                    <input type="text" class="form-control" placeholder="Post Box" name="postbox">
+                                </div>
                             </div>
                         </div>
                         <div class="col-md-6">
                             <div class="form-group">
                                 <label>TAX ID</label>
-                                <input type="number" class="form-control" placeholder="Tax Id" name="taxid">
+                                <div class="input-group">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text"><i class="fas fa-box"></i></span>
+                                    </div>
+                                    <input type="number" class="form-control" placeholder="Tax Id" name="taxid">
+                                </div>
                             </div>
                         </div>
                     </div>
 
                 </div>
-                <!-- /.card-body -->
 
                 <div class="card-footer">
                   <button type="submit" class="btn btn-primary">Submit</button>
@@ -130,7 +176,6 @@
       </div>
 
     </section>
-
 
 </div>
 @endsection
