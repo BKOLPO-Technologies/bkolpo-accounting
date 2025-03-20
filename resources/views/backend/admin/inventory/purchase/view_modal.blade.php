aa6128800483c902e1db33bfe7dfedf3ee2b1af4
RJ ZIM <rjzim01@gmail.com>
2025-03-20 16:02:10 +0600

    purchase_view_modal_update

--- resources/views/backend/admin/inventory/purchase/view_modal.blade.php
+++ resources/views/backend/admin/inventory/purchase/view_modal.blade.php
@@ -1,7 +1,7 @@
 <!-- Purchase Details Modal -->
 <div class="modal fade" id="purchaseDetailsModal" tabindex="-1" aria-labelledby="purchaseDetailsModalLabel" 
      data-backdrop="static" data-keyboard="false">
-    <div class="modal-dialog modal-lg">
+    <div class="modal-dialog modal-xl">
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title" id="purchaseDetailsModalLabel">Purchase Details</h5>
