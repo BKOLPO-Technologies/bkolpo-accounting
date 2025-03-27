36e5f81ed0fd5e284966a958994ae446a05b08ef
RJ ZIM <rjzim01@gmail.com>
2025-03-27 17:03:01 +0600

    client_update_2

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -3,15 +3,25 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Traits\CalculatesClientPurchases;
+
 
 class Client extends Model
 {
+    use CalculatesClientPurchases;
+
     protected $guarded = [];
 
     // Define the relationship with the Sale model
     public function sales()
     {
         return $this->hasMany(Sale::class);
+    }
+
+    // Define the relationship with payments
+    public function payments()
+    {
+        return $this->hasMany(ProjectReceipt::class);
     }
 
     public function quotations()
