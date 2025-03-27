480f5bfa502795bc86dc69a41e53d6f9bf153846
nazrulislam-CSE <nsuzon02@gmail.com>
2025-03-27 17:12:08 +0600

    erer

--- resources/views/backend/admin/inventory/sales/index.blade.php
+++ resources/views/backend/admin/inventory/sales/index.blade.php
@@ -53,8 +53,8 @@
                                                     <td>{{ $loop->iteration }}</td>
                                                     <td>{{ $sale->invoice_no }}</td>
                                                     <td>{{ $sale->project->reference_no ?? '' }}</td>
-                                                    <td>{{ \Carbon\Carbon::parse($sale->invoice_date)->format('d F Y') }}</td>
                                                     <td>{{ $sale->client->name ?? 'N/A' }}</td> 
+                                                    <td>{{ \Carbon\Carbon::parse($sale->invoice_date)->format('d F Y') }}</td>
                                                     <td>{{ bdt() }} {{ number_format($sale->total, 2) }}</td> 
                                                     <td>{{ bdt() }} {{ number_format($sale->paid_amount ?? 'N/A', 2) }}</td> 
                                                     <td>{{ bdt() }} {{ number_format($sale->total-$sale->paid_amount, 2) }}</td> 
