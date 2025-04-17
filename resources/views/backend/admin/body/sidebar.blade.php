ccb728f703c798f7da3f281021936e8338c09c60
nazrulislam-CSE <nsuzon02@gmail.com>
2025-04-17 16:38:56 +0600

    update sideber

--- resources/views/backend/admin/body/sidebar.blade.php
+++ resources/views/backend/admin/body/sidebar.blade.php
@@ -340,38 +340,109 @@
               <a href="#" class="nav-link {{ $isReportActive ? 'active' : '' }}">
                   <i class="far fa-circle nav-icon"></i>
                   <p>
-                      Accounts
+                      Trial Balance
                       <i class="fas fa-angle-left right"></i>
                   </p>
               </a>
-              <ul class="nav nav-treeview">
-                @can('report-list')  
-                <li class="nav-item">
-                  <a href="{{ route('report.index') }}" class="nav-link {{ Route::is('report.index') ? 'active' : '' }}">
-                      <i class="far fa-circle nav-icon"></i>
-                      <p>Report</p>
-                  </a>
-                </li>
-                @endcan
-
-                @can('trial-balnce-report')  
-                <li class="nav-item">
-                  <a href="{{ route('report.trial.balance') }}" class="nav-link {{ Route::is('report.trial.balance') ? 'active' : '' }}">
-                    <i class="far fa-circle nav-icon"></i>
-                    <p>Trial Balance</p>
-                  </a>
-                </li>
-                @endcan
-
-                @can('balance-shit-report')  
-                <li class="nav-item">
-                  <a href="{{ route('report.balance.sheet') }}" class="nav-link {{ Route::is('report.balance.sheet') ? 'active' : '' }}">
-                    <i class="far fa-circle nav-icon"></i>
-                    <p>Balance Sheet</p>
-                  </a>
-                </li>
-                @endcan
-              </ul>
+            </li>
+            <li class="nav-item {{ $isReportActive ? 'menu-open' : '' }}">
+              <a href="#" class="nav-link {{ $isReportActive ? 'active' : '' }}">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>
+                      Balance Sheet
+                      <i class="fas fa-angle-left right"></i>
+                  </p>
+              </a>
+            </li>
+            <li class="nav-item {{ $isReportActive ? 'menu-open' : '' }}">
+              <a href="#" class="nav-link {{ $isReportActive ? 'active' : '' }}">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>
+                      Profit and Loss
+                      <i class="fas fa-angle-left right"></i>
+                  </p>
+              </a>
+            </li>
+            <li class="nav-item {{ $isReportActive ? 'menu-open' : '' }}">
+              <a href="#" class="nav-link {{ $isReportActive ? 'active' : '' }}">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>
+                      Receipts & Payments / Cash Flow
+                      <i class="fas fa-angle-left right"></i>
+                  </p>
+              </a>
+            </li>
+            <li class="nav-item {{ $isReportActive ? 'menu-open' : '' }}">
+              <a href="#" class="nav-link {{ $isReportActive ? 'active' : '' }}">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>
+                      Daybook
+                      <i class="fas fa-angle-left right"></i>
+                  </p>
+              </a>
+            </li>
+            <li class="nav-item {{ $isReportActive ? 'menu-open' : '' }}">
+              <a href="#" class="nav-link {{ $isReportActive ? 'active' : '' }}">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>
+                      Statement
+                      <i class="fas fa-angle-left right"></i>
+                  </p>
+              </a>
+            </li>
+            <li class="nav-item {{ $isReportActive ? 'menu-open' : '' }}">
+              <a href="#" class="nav-link {{ $isReportActive ? 'active' : '' }}">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>
+                      Groupwise Statement
+                      <i class="fas fa-angle-left right"></i>
+                  </p>
+              </a>
+            </li>
+            <li class="nav-item {{ $isReportActive ? 'menu-open' : '' }}">
+              <a href="#" class="nav-link {{ $isReportActive ? 'active' : '' }}">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>
+                      Bills Receivable
+                      <i class="fas fa-angle-left right"></i>
+                  </p>
+              </a>
+            </li>
+            <li class="nav-item {{ $isReportActive ? 'menu-open' : '' }}">
+              <a href="#" class="nav-link {{ $isReportActive ? 'active' : '' }}">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>
+                      Bills Payable
+                      <i class="fas fa-angle-left right"></i>
+                  </p>
+              </a>
+            </li>
+            <li class="nav-item {{ $isReportActive ? 'menu-open' : '' }}">
+              <a href="#" class="nav-link {{ $isReportActive ? 'active' : '' }}">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>
+                      Purchase And Sales
+                      <i class="fas fa-angle-left right"></i>
+                  </p>
+              </a>
+            </li>
+            <li class="nav-item {{ $isReportActive ? 'menu-open' : '' }}">
+              <a href="#" class="nav-link {{ $isReportActive ? 'active' : '' }}">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>
+                      Sales Report
+                      <i class="fas fa-angle-left right"></i>
+                  </p>
+              </a>
+            </li>
+            <li class="nav-item {{ $isReportActive ? 'menu-open' : '' }}">
+              <a href="#" class="nav-link {{ $isReportActive ? 'active' : '' }}">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>
+                      Purchase Report
+                      <i class="fas fa-angle-left right"></i>
+                  </p>
+              </a>
             </li>
             @endcan
           </ul>
