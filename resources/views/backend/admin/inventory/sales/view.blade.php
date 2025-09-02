f01229c9099d2384470501ff55c41fc52028a495
nazrulislam-CSE <nsuzon02@gmail.com>
2025-09-02 14:07:42 +0600

    purchases invoice update vat tax

--- resources/views/backend/admin/inventory/sales/view.blade.php
+++ resources/views/backend/admin/inventory/sales/view.blade.php
@@ -98,7 +98,7 @@
                                         </div>
                                         <div class="col-sm-4 invoice-col">
                                             <b>Invoice :- {{ $sale->invoice_no }}</b><br>
-                                            <b>Reference :- {{ $sale->project->reference_no }}</b><br>
+                                            <b>Reference :- {{ $sale->project->reference_no ?? '' }}</b><br>
                                         </div>
                                     </div>
 
